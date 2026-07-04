e79fdb486d9bce588d272c7f71f20257e03316a5
FAndresG02 <andresfranklin02@gmail.com>
2026-07-04 03:44:56 -0500

    componente about

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About {}
+export class About {
+  isModalOpen = false;
+  selectedImageSrc = '';
+
+  openModal(imageSrc: string): void {
+    this.selectedImageSrc = imageSrc;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.selectedImageSrc = '';
+  }
+
+  onOverlayClick(event: MouseEvent): void {
+    if ((event.target as HTMLElement).classList.contains('lightbox-overlay')) {
+      this.closeModal();
+    }
+  }
+}
